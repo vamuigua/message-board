629c73ba6da50830a114e9d875ffa62b88f4d877
Victor Allen <vamuigua@gmail.com>
2017-06-10 15:08:32 +0300

    Modified model route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
+// var questions = [{
+//   id:1,
+//   content:"lalalala",
+//   author:"ksksksk",
+//   notes:"hddhdh"
+// }];
+
 export default Ember.Route.extend({
+  model() {
+    // return questions;
+    return this.store.findAll('data');
+  },
 });
